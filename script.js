0e28c560f363e4bc79fccb8abd0f2feb8be34252
unknown <vladimir.novikov2000@mail.ru>
2020-09-07 13:48:39 +0300

    code

---
+++ script.js
@@ -0,0 +1,122 @@
+let plus = document.querySelector('.plus');
+let addButton = document.querySelector('.add');
+addButton.addEventListener('click', () => {
+    let newElement = document.createElement('div');
+    newElement.innerHTML = option.innerHTML;
+    newElement.classList.add('function');
+    newElement.draggable = 'true';
+    frame.append(newElement);
+
+    let deleteButton = newElement.querySelector('.frameDelete');
+    deleteButton.addEventListener('click', () => {
+        removeElements(newElement);
+    });
+});
+addButton.addEventListener('mouseover', () => {
+    addButton.style.backgroundColor = '#9953F1';
+    plus.style.backgroundColor = '#AA68FE';
+});
+
+addButton.addEventListener('mouseout', () => {
+    addButton.style.backgroundColor = '#833AE0';
+    plus.style.backgroundColor = '#9953F1';
+});
+let option = document.querySelector('.function');
+let deleteButton = document.querySelector('.frameDelete');
+function removeElements(element) {
+    let optionElements = document.querySelectorAll('.function');
+    if (optionElements.length > 1) {
+        element.remove();
+    }
+}
+deleteButton.addEventListener('click', () => {
+    removeElements(option);
+})
+let sortDown = document.querySelector('.down');
+let sortUp = document.querySelector('.up');
+let inputData = document.querySelector('.inputText');
+sortDown.addEventListener('click', () => {
+    if(sortDown.style.display = 'block') {
+        sortDown.style.display = 'none';
+        sortUp.style.display = 'block';
+    }
+    let newArr = [];
+    let inputValues = document.querySelectorAll('.inputText');
+  
+    for ( let i = 0; i < inputValues.length; i++){
+      newArr.push(inputValues[i].value);
+    }
+    newArr.sort((a, b) => {
+      if (a < b) {
+          return -1;
+      }
+      if (a > b) {
+          return 1;
+      }
+      return 0;
+  });
+    for (let k = 0; k < newArr.length; k++) {
+      inputValues[k].value = newArr[k];
+    }
+  });
+  sortUp.addEventListener('click', () => {
+    if (sortUp.style.display = 'block') {
+        sortUp.style.display = 'none';
+        sortDown.style.display = 'block';
+    }
+    let newArr = [];
+    let inputValues = document.querySelectorAll('.inputText');
+  
+    for ( let i = 0; i < inputValues.length; i++){
+      newArr.push(inputValues[i].value);
+    }
+    newArr.sort((a, b) => {
+      if (a < b) {
+          return 1;
+      }
+      if (a > b) {
+          return -1;
+      }
+      return 0;
+  });
+    for (let k = 0; k < newArr.length; k++) {
+      inputValues[k].value = newArr[k];
+    }
+  });
+
+
+let frame = document.querySelector('.frame');
+frame.addEventListener('dragstart', (event) => {
+    event.target.style.backgroundColor = '#FFDC40';
+    event.target.style.mixBlendMode = 'multiply';
+    event.target.classList.add('selected');
+});
+frame.addEventListener('drag', (event) => {
+    event.target.style.backgroundColor = '#E4E4E4';
+});
+frame.addEventListener('dragend', (event) => {
+    event.target.style.backgroundColor = 'white';
+    event.target.classList.remove('selected');
+});
+
+frame.addEventListener('dragover', (event) => {
+    event.preventDefault();
+
+    let activeElement = frame.querySelector('.selected');
+    let currentElement = event.target;
+
+    let isMoveable = activeElement !== currentElement &&
+    currentElement.classList.contains('function');
+
+    if (!isMoveable) {
+        return;
+    }
+
+    let nextElement;
+    if (currentElement === activeElement.nextElementSibling) {
+        nextElement = currentElement.nextElementSibling;
+    } else {
+        nextElement = currentElement;
+    }
+    frame.insertBefore(activeElement, nextElement);
+});
